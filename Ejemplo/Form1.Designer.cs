3fed42dee555c6cb05f2336f28747abaa84dd3f3
Jonathan <jonasshorty18@gmail.com>
2020-07-22 18:30:17 -0300

    inicio

---
+++ Ejemplo/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Ejemplo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbnombre = new System.Windows.Forms.TextBox();
+            this.tbapellido = new System.Windows.Forms.TextBox();
+            this.tbdni = new System.Windows.Forms.TextBox();
+            this.btncrear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // tbnombre
+            // 
+            resources.ApplyResources(this.tbnombre, "tbnombre");
+            this.tbnombre.Name = "tbnombre";
+            // 
+            // tbapellido
+            // 
+            resources.ApplyResources(this.tbapellido, "tbapellido");
+            this.tbapellido.Name = "tbapellido";
+            // 
+            // tbdni
+            // 
+            resources.ApplyResources(this.tbdni, "tbdni");
+            this.tbdni.Name = "tbdni";
+            // 
+            // btncrear
+            // 
+            resources.ApplyResources(this.btncrear, "btncrear");
+            this.btncrear.Name = "btncrear";
+            this.btncrear.UseVisualStyleBackColor = true;
+            this.btncrear.Click += new System.EventHandler(this.btncrear_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // btnmostrar
+            // 
+            resources.ApplyResources(this.btnmostrar, "btnmostrar");
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // btneliminar
+            // 
+            resources.ApplyResources(this.btneliminar, "btneliminar");
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnmostrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btncrear);
+            this.Controls.Add(this.tbdni);
+            this.Controls.Add(this.tbapellido);
+            this.Controls.Add(this.tbnombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbnombre;
+        private System.Windows.Forms.TextBox tbapellido;
+        private System.Windows.Forms.TextBox tbdni;
+        private System.Windows.Forms.Button btncrear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.Button btneliminar;
+    }
+}
+
---
+++ Ejemplo/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int id = -1;
+
+        private void crearPersona()
+        {
+            using(VolviendoEntities db = new VolviendoEntities())
+            {
+                try
+                {
+                    Persona per = new Persona();
+                  
+                    per.Nombre = tbnombre.Text;
+                    per.Apellido = tbapellido.Text;
+                    per.DNI = Convert.ToInt32(tbdni.Text);
+
+                    db.Persona.Add(per);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Exito");
+
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.Message, "ERROR", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+                
+
+            }
+        }
+
+        private void btncrear_Click(object sender, EventArgs e)
+        {
+            crearPersona();
+            mostrar();
+        }
+
+        private void mostrar()
+        {
+            VolviendoEntities db = new VolviendoEntities();
+            dataGridView1.DataSource = db.Persona.ToList();
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            mostrar();
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                id = Convert.ToInt32(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[3].Value.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(id != -1)
+                {
+                    using (VolviendoEntities db = new VolviendoEntities())
+                    {
+                        Persona per = new Persona();
+                        per = db.Persona.Find(id);
+                        db.Persona.Remove(per);
+                        db.SaveChanges();
+
+                        MessageBox.Show("Eliminado correctamente");
+                        mostrar();
+                    }
+                }else
+                {
+                    MessageBox.Show("No se encontro el elemento");
+                    MessageBox.Show(id.ToString());
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error");
+               
+            }
+        }
+    }
+}
